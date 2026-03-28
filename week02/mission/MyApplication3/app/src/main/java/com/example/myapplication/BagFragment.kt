c4f99ce2e318a7da26cf9372bdf4d5e98a6bb5e1
KimHoGyun <khg010122@naver.com>
2026-03-28 12:06:30 +0900

    s

---
+++ week02/mission/MyApplication3/app/src/main/java/com/example/myapplication/BagFragment.kt
@@ -0,0 +1,52 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.myapplication.databinding.FragmentBagBinding
+
+class BagFragment : Fragment() {
+
+    private var _binding: FragmentBagBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentBagBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnOrder.setOnClickListener {
+            if (isBagEmpty()) {
+                navigateToShop()
+            }
+        }
+    }
+
+    private fun isBagEmpty(): Boolean {
+        return true
+    }
+
+    private fun navigateToShop() {
+        requireActivity().supportFragmentManager.beginTransaction()
+            .replace(R.id.main_fragmentContainer, BuyFragment())
+            .commit()
+
+        requireActivity().findViewById<com.google.android.material.bottomnavigation.BottomNavigationView>(
+            R.id.main_bnv
+        ).selectedItemId = R.id.buy //여기 고쳐야 함
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ week02/mission/MyApplication3/app/src/main/java/com/example/myapplication/WishListFragment.kt
@@ -0,0 +1,32 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.myapplication.databinding.ActivityMainBinding
+
+class WishListFragment : Fragment() {
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = ActivityMainBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
