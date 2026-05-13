82847e546ff39d963ea3c95fcca7a9fb966c29a4
Neouul <fluctu0q0@gmail.com>
2026-05-13 13:55:43 +0900

    chore: week07 - 미션 초기 세팅

---
+++ week07/mission/app/src/main/java/com/neouul/umc10android/week07/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.neouul.umc10android.week07
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.neouul.umc10android.week07.ui.theme.UMC10thWeek07ComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            UMC10thWeek07ComposeTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    UMC10thWeek07ComposeTheme {
+        Greeting("Android")
+    }
+}
